ee0533f0dc62fd28166d189e8b35cb4bb589e24a
Balint Bela <bela.balint@msg-systems.com>
2016-10-12 16:46:05 +0300

    UserDAO documented

--- spareparts/sparepartsEJB/src/main/java/daos/UserDAO.java
+++ spareparts/sparepartsEJB/src/main/java/daos/UserDAO.java
@@ -15,40 +15,58 @@ import org.apache.log4j.Logger;
 
 import model.User;
 
+/**
+ * This is a Data Transfer Object for User entity
+ */
 @Stateless(name = "UserDAO", mappedName = "ejb.UserDAO")
 public class UserDAO {
-	
-	final static Logger logger = Logger.getLogger(UserDAO.class);
-	
+
+	static final Logger logger = Logger.getLogger(UserDAO.class);
+
 	@PersistenceContext(unitName = "spareparts")
 	private EntityManager entityManager;
-	
-	public List<User> getAllUsers(){
-		TypedQuery<User> tq = null;
-		try{
+
+	/**
+	 * This method returns all the records from the 'user' table;
+	 */
+	public List<User> getAllUsers() {
+		try {
 			CriteriaBuilder cb = entityManager.getCriteriaBuilder();
 			CriteriaQuery<User> cq = cb.createQuery(User.class);
 			Root<User> root = cq.from(User.class);
 			CriteriaQuery<User> allEntities = cq.select(root);
-			tq = entityManager.createQuery(allEntities);
-			
-		} catch(PersistenceException e){
+			TypedQuery<User> tq = entityManager.createQuery(allEntities);
+			return tq.getResultList();
+
+		} catch (PersistenceException e) {
 			logger.error("Exception in getAllUsers: " + e);
+			return null;
 		}
-		return tq.getResultList();
+
 	}
-	
-	public User login(String userName, String password){
-		try{
+
+	/**
+	 * This method verifies if the user name exists with the corresponding
+	 * password
+	 * 
+	 * @param userName
+	 *            The user name to be verified.
+	 * @param password
+	 *            The password for the given user.
+	 * @return User type model if the login data is correct
+	 * @return 'null' in case of invalid password or inexistent user
+	 */
+	public User login(String userName, String password) {
+		try {
 			TypedQuery<User> query = entityManager.createQuery(
 					"SELECT u from User u WHERE u.userName = :userName AND u.password = :password", User.class);
 			query.setParameter("userName", userName);
 			query.setParameter("password", password);
 			return query.getSingleResult();
-		} catch (PersistenceException e){
+		} catch (PersistenceException e) {
 			logger.error("Exception in login: " + e);
 			return null;
 		}
 	}
-	
+
 }
